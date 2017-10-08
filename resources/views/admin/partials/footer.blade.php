61c851ed467506deb7140e3fa8ff521a172b3578
“liunxsugyli” <3101967255@qq.com>
2017-10-08 12:07:03 +0800

    10.8_7

--- resources/views/admin/partials/footer.blade.php
+++ resources/views/admin/partials/footer.blade.php
@@ -5,5 +5,5 @@
         <strong>Version</strong>&nbsp;&nbsp; {!! config('admin.version') !!}
     </div>
     <!-- Default to the left -->
-    <strong>Powered by <a href="https://github.com/z-song/laravel-admin" target="_blank">laravel-admin</a></strong>
+    <strong>Powered by sugyli</strong>
 </footer>
