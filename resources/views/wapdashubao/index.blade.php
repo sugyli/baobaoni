3dfa17ed04daf9165263bcdf7386980c8e15c8f3
“liunxsugyli” <3101967255@qq.com>
2017-10-13 10:53:05 +0800

    10.13_10

--- resources/views/wapdashubao/index.blade.php
+++ resources/views/wapdashubao/index.blade.php
@@ -3,10 +3,13 @@
 @section('content')
 
 @include('wapdashubao.include.header')
-<div style="padding-top: 45px;" v-bind:style="'width:'+ screen_width + 'px;'">
-  @include('wapdashubao.include.index-top')
+<div v-bind:style="'width:'+ double_screen_width +
+                  'px;transition-duration:.5s;transform:translate3d('+
+                  position + 'px,0px,0px);'">
+  <div style="padding-top: 45px;" v-bind:style="'width:'+ screen_width + 'px;'">
+    @include('wapdashubao.include.index-top')
+  </div>
 </div>
-
 
 
 
