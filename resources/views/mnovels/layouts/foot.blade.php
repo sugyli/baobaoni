713f426c1f11e7b6d67476997bf6ec68d05cd7fd
“liunxsugyli” <3101967255@qq.com>
2017-11-12 05:30:00 +0800

    11_12_4

--- resources/views/mnovels/layouts/foot.blade.php
+++ resources/views/mnovels/layouts/foot.blade.php
@@ -1,3 +1,3 @@
 <section class="foot">
-  <a href="javasrcipt:" >客户端</a>|<a href="config('app.url_web')" >电脑版</a>
+  <a href="javasrcipt:" >客户端</a>|<a href="{{config('app.url_web')}}" >电脑版</a>
 </section>
