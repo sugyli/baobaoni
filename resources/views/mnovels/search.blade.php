f4bf79a9c15842492cfbccacd837d3b111a4d6c7
“liunxsugyli” <3101967255@qq.com>
2017-11-11 21:27:27 +0800

    11-11-2

--- resources/views/mnovels/search.blade.php
+++ resources/views/mnovels/search.blade.php
@@ -5,5 +5,12 @@
 @section('content')
 <scroll-search
 ></scroll-search>
-
+<div style="padding-top:150px;text-align:center;" id="solist">
+  <input type="button" style="font-size:20px" onclick="location.href= '/search'" value="被百度转码点击下切换">
+</div>
+@endsection
+@section('subscripts')
+<script>
+$('#solist').hide();
+</script>
 @endsection
