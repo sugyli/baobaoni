a70e22610aa9b2d1265cb4149b2efdbc7a617ad9
“liunxsugyli” <3101967255@qq.com>
2017-11-12 18:03:57 +0800

    11-12-37

--- bootstrap/helpers.php
+++ bootstrap/helpers.php
@@ -9,7 +9,39 @@ if (!function_exists('formatTime')) {
     return \Carbon\Carbon::createFromFormat('Y-m-d H:i:s', $t)->diffForHumans();
   }
 }
+/**
+ * 求取字符串位数（非字节），以UTF-8编码长度计算
+ *
+ * @param string $string 需要被计算位数的字符串
+ * @return int
+ * @author Seven Du <lovevipdsw@vip.qq.com>
+ **/
 
+if (!function_exists('getstrlength')) {
+  function getstrlength($string)
+  {
+      $length = strlen($string);
+      $index  = $num = 0;
+      while ($index < $length) {
+          $str = $string[$index];
+          if ($str < "\xC0") {
+              $index += 1;
+          } elseif ($str < "\xE0") {
+              $index += 2;
+          } elseif ($str < "\xF0") {
+              $index += 3;
+          } elseif ($str < "\xF8") {
+              $index += 4;
+          } elseif ($str < "\xFC") {
+              $index += 5;
+          } else {
+              $index += 6;
+          }
+          $num += 1;
+      }
+      return $num;
+  }
+}
 //获取小说内容
 if (!function_exists('saveOrGetTxtData')) {
 
