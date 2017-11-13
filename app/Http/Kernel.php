9e2999030a6efe2ee835c745bff395a85ed5a83f
“liunxsugyli” <3101967255@qq.com>
2017-11-13 19:51:33 +0800

    11-13-34

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -47,6 +47,7 @@ class Kernel extends HttpKernel
             \App\Http\Middleware\VerifyCsrfToken::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
         ],
+        
         'api' => [
             'throttle:60,1',
             'bindings',
