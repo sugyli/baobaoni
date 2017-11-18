6cbea7d4ae90c27fec1c47334aee6b68668bb817
“liunxsugyli” <3101967255@qq.com>
2017-11-18 20:29:12 +0800

    11.18.17

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -27,7 +27,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
 
-      $schedule->command('pushbook:xiaoshuo')->everyFiveMinutes();
+      $schedule->command('pushbook:xiaoshuo');
       // $schedule->command('inspire')
         //          ->hourly();
 
