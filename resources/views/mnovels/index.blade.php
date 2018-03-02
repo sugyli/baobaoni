38b39b38d98d45a4d7593b79d8d25e48b282b043
“liunxsugyli” <3101967255@qq.com>
2018-03-02 16:07:04 +0800

    3.2_1

--- resources/views/mnovels/index.blade.php
+++ resources/views/mnovels/index.blade.php
@@ -14,7 +14,7 @@
 </div>
 <div id="root" class="container-warp">
   <section class="i-top">
-    <a href="/newsearch">
+    <a href="javascript:" onclick="location.href= '/newsearch'">
   		<div class="i-top-search">输入书名/作者/关键字</div>
   	</a>
     <div class="i-top-nav">
