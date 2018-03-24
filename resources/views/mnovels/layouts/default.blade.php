968cfc63ad742629a3eb38f47e0609c73ca2edc7
“liunxsugyli” <3101967255@qq.com>
2018-03-24 19:56:04 +0800

    3.24.1

--- resources/views/mnovels/layouts/default.blade.php
+++ resources/views/mnovels/layouts/default.blade.php
@@ -16,7 +16,7 @@
     <script src="/js/jquery.min.js"></script>
     <script src="/js/layer_mobile/layer.js"></script>
     <script src="/js/axios.min.js"></script>
-    <script src="/js/quanju_16.js"></script>
+    <script src="/js/quanju_17.js"></script>
   </head>
   <body>
     <script>
